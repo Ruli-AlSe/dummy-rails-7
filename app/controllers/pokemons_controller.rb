d34c406df1832528886d901c89720dd44dc56c1b
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-12-27 03:02:30 -0600

    Create model, controller and migration for pokemons

---
+++ app/controllers/pokemons_controller.rb
@@ -0,0 +1,16 @@
+class PokemonsController < ApplicationController
+  def index
+  end
+
+  def search
+    pokemon = PokemonLocatorService.new(params[:name]).call
+
+    if pokemon
+      flash[:notice] = "#{pokemon.name}'s number is #{pokemon.number}"
+    else
+      flash[:error] = "Pokemon not found"
+    end
+
+    redirect_to root_path
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_27_013814) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_27_031243) do
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -32,6 +32,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_27_013814) do
     t.string "description"
     t.boolean "published", default: false
     t.boolean "visible", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pokemons", force: :cascade do |t|
+    t.string "name"
+    t.string "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
